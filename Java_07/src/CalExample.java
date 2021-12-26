b7886fff0bd605827651f3d3a800fd8e380bfd65
wjche <wjcheon96@gmail.com>
2021-12-26 23:47:06 +0900

    클래스

---
+++ Java_07/src/CalExample.java
@@ -0,0 +1,7 @@
+public class CalExample {
+    public static void main(String[] args) {
+        CalSample myCalc = new CalSample();
+        myCalc.execute();       //CalSample 의 execute() 메소드 호출
+
+    }
+}
---
+++ Java_07/src/thisSampleExample.java
@@ -0,0 +1,9 @@
+public class thisSampleExample {
+    public static void main(String[] args) {
+        thisSample myCar = new thisSample("포르쉐");
+        thisSample yourCar = new thisSample("벤츠");
+
+        myCar.run();
+        yourCar.run();
+    }
+}
