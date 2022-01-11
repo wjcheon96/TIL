889a48f81b12cb4579c59b8cf7293c82e5d3788a
wjche96 <wjcheon96@gmail.com>
2022-01-11 19:11:57 +0900

    상속

--- java_08/src/main.java
+++ java_08/src/main.java
@@ -3,5 +3,7 @@ public class main {
         System.out.println("test");
         System.out.println("test2");
         System.out.println("test3");
+        System.out.println("test4");
+        System.out.println("test5");
     }
 }
