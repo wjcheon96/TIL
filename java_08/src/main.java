5a2972d7b03b975a988bd6d39a4a779e472aa303
wjche96 <wjcheon96@naver.com>
2022-01-11 19:04:27 +0900

    modified:   java_08/src/main.java

--- java_08/src/main.java
+++ java_08/src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test2");
+        System.out.println("test3");
     }
 }
