1cf8473c0d45e364f02601a26ce015a3a5177fe9
wjche96 <wjcheon96@gmail.com>
2022-01-18 17:42:00 +0900

    인터페이스

---
+++ Java_09/src/TV/RemoteControlExample.java
@@ -0,0 +1,9 @@
+package TV;
+
+public class RemoteControlExample {
+    public static void main(String[] args) {
+        RemoteControl rc;
+        rc = new Television();
+        rc = new Audio();
+    }
+}
